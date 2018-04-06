dfce5c4db2f81ef866c580e6e803a182867abfde
Mangosh <leftpicture@gmail.com>
2018-04-06 17:42:58 +0100

    removing title in vimeo

--- templates/views/helpers/index.js
+++ templates/views/helpers/index.js
@@ -197,7 +197,7 @@ module.exports = function () {
 
 	_helpers.getVideoURL = function (a) {
 		if(a.includes("vimeo")){
-			return "https://player.vimeo.com/video/"+_.split(a,".com/")[1];
+			return "https://player.vimeo.com/video/"+_.split(a,".com/")[1]+"?title=false";
 		}else{
 			return "https://www.youtube.com/embed/"+_.split(a,"v=")[1]+"?enablejsapi=1&showinfo=0";
 		}
