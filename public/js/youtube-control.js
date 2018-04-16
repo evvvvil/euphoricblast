b609a2baab21f07bb5dfde6c1a6448cac7e1be1a
Mangosh <leftpicture@gmail.com>
2018-04-16 17:37:26 +0100

    remove cdn youtube and vimeo

--- public/js/youtube-control.js
+++ public/js/youtube-control.js
@@ -13,7 +13,7 @@ function onPlayerReady(event) {
 var player;
 $(window).on("load", function() {
 var tag = document.createElement('script');
-tag.src = "/js/vimeo-player.js";
+tag.src = "/js/youtube-player.js";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
