7228ae7cc3082f75f9093db7cfe59da7f70902e1
Mangosh <leftpicture@gmail.com>
2018-04-16 18:55:10 +0100

    video player resizer 6

--- public/js/video-player-resizer.js
+++ public/js/video-player-resizer.js
@@ -8,11 +8,8 @@ $( window ).resize(function() {
 
 function checkSize(){
 var wid=$(".col-lg-4.video-col").outerWidth();
-	console.log("wdith "+wid);
 	if(wid<=768){
-		console.log("resizing video container!");
-		$(".col-lg-4.video-col").height(Math.floor((wid-30)*(9/16)));
-		console.log("resized to: "+Math.floor((wid-30)*(9/16)));
+		$(".col-lg-4.video-col").height(Math.ceil((wid-30)*(9/16)));		
 	}
 }
 
