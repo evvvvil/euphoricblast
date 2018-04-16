2dc843d9d1e42f9500e15ca7d538b1681c400571
Mangosh <leftpicture@gmail.com>
2018-04-16 20:53:15 +0100

    vimeo fix image height

--- public/js/video-player-resizer.js
+++ public/js/video-player-resizer.js
@@ -7,9 +7,10 @@ $( window ).resize(function() {
 });
 
 function checkSize(){
-var wid=$(".col-lg-4.video-col").outerWidth();
-console.log("width: "+wid);
+var wid=$(window).outerWidth();
+//console.log("width: "+wid);
 	if(wid<=768){
+		var curWid=$(".col-lg-4.video-col").outerWidth();
 		$(".col-lg-4.video-col").height(Math.ceil((wid-30)*(9/16)));		
 	}
 }
