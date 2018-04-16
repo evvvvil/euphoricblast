3a7f1c998f49ea95f7788f6b0f17d312788f06ed
Mangosh <leftpicture@gmail.com>
2018-04-16 22:05:31 +0100

    MOB FIX

--- public/js/video-player-resizer.js
+++ public/js/video-player-resizer.js
@@ -12,5 +12,7 @@ var wid=$(window).outerWidth();
 	if(wid<=768){
 		var curWid=$(".col-lg-4.video-col").outerWidth();
 		$(".col-lg-4.video-col").height(Math.ceil((wid-30)*(9/16)));		
+	}else
+		$(".col-lg-4.video-col").height("100%");		
 	}
 }
