1c16c55083df2f375ab5dab3f2878dbe4af3bb9d
Mangosh <leftpicture@gmail.com>
2018-04-21 20:12:59 +0100

    flooring

--- public/js/video-player-resizer.js
+++ public/js/video-player-resizer.js
@@ -10,10 +10,10 @@ function checkVideoSize(){
 	var wid=$(window).outerWidth();
 	if(wid<=768 && wid>480){
 		var curWid=$(".col-lg-4.video-col").outerWidth();
-		$(".col-lg-4.video-col").height(Math.ceil((wid-30)*(9/16)));		
+		$(".col-lg-4.video-col").height(Math.floor((wid-30)*(9/16)));		
 	}
 	else if(wid<=480){
 		var curWid=$(".col-lg-4.video-col").outerWidth();
-		$(".col-lg-4.video-col").height(Math.floor((wid-30)*(9/16)));
+		$(".col-lg-4.video-col").height(Math.ceil((wid-30)*(9/16)));
 	}
 }
