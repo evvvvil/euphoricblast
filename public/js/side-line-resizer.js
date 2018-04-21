4fae122a16f1034a7ba7c56b101d7041ef69da63
Mangosh <leftpicture@gmail.com>
2018-04-21 13:48:23 +0100

    swapped :after for side-line div

--- public/js/side-line-resizer.js
+++ public/js/side-line-resizer.js
@@ -10,7 +10,7 @@ function checkLineSize(){
 	var wid=$(window).outerWidth();
 	console.log("width: "+wid);
 	if(wid<=390){
-		$(".side-line").each(function{
+		$(".side-line").each(function( index, value ){
 			var curHeight=$(this).closest().children('.project-image-container').outerHeight();
 			console.log("curHeight: "+curHeight);
 			$(this).height(curHeight+8);
