2e250cb30274780e8f1c5928a207254c85b8d4ee
Mangosh <leftpicture@gmail.com>
2018-04-21 20:00:56 +0100

    flooring

--- public/js/video-player-resizer.js
+++ public/js/video-player-resizer.js
@@ -11,7 +11,7 @@ var wid=$(window).outerWidth();
 //console.log("width: "+wid);
 	if(wid<=768){
 		var curWid=$(".col-lg-4.video-col").outerWidth();
-		$(".col-lg-4.video-col").height(Math.ceil((wid-30)*(9/16)));		
+		$(".col-lg-4.video-col").height(Math.floor((wid-30)*(9/16)));		
 	}
 	/*else{
 		$(".col-lg-4.video-col").height("100%");		
