eab38eb662c83a9ca26c55b169ac87dbc8c752c0
Mangosh <leftpicture@gmail.com>
2018-05-05 20:54:23 +0100

    yt fix

--- public/js/youtube-control.js
+++ public/js/youtube-control.js
@@ -1,21 +1,21 @@
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('euphoric-video', {
-        /*events: {
+        events: {
 		   'onReady': onPlayerReady
-        }*/
+        }
     });
 }
 function onPlayerReady(event) {
 	console.log("broski loaded youtube player!");
-	event.target.setVolume(0);
+	/*event.target.setVolume(0);*/
 	var fn = function(){console.log("broski playing!!!");event.target.playVideo(); }
         setTimeout(fn, 5000);
 	//event.target.playVideo();
 }
 $(window).on("load", function() {
-/*var tag = document.createElement('script');
+var tag = document.createElement('script');
 tag.src = "/js/youtube-player.js";
 var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);*/
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
