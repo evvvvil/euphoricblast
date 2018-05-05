ec4af77b40051eefc4432d9f1aa3b5a7206c1562
Mangosh <leftpicture@gmail.com>
2018-05-05 20:24:15 +0100

    youtube fix

--- templates/views/helpers/index.js
+++ templates/views/helpers/index.js
@@ -330,7 +330,7 @@ module.exports = function () {
 			}
 			return result;
 		}else{
-			return "https://www.youtube.com/embed/"+_.split(a,"v=")[1]+"?enablejsapi=1&showinfo=0?autoplay=1";
+			return "https://www.youtube.com/embed/"+_.split(a,"v=")[1]+"?enablejsapi=1&showinfo=0&autoplay=1";
 		}
 	};
 	_helpers.getLoopVideoURL = function (a) {
