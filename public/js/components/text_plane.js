5a2191ec8713c43b0a99dfd75ba36ad4aff24556
Mangosh <leftpicture@gmail.com>
2019-02-18 12:10:49 +0000

    Update text_plane.js

--- public/js/components/text_plane.js
+++ public/js/components/text_plane.js
@@ -32,13 +32,17 @@ AFRAME.registerComponent('text_plane', {
 		evil.createAnimation("fadein-"+data.id,el,"scale","fade-in","fade-stop","1 1 1","1000");		
 		evil.createAnimation("fadeout-"+data.id,el,"scale","fade-out","fade-stop","0 0 0","1000");
 		var dummyText='Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet';
+		el.setAttribute("geometry","primitive:plane;width:auto;height:auto");
+			
 		el.setAttribute("text","value",dummyText);
+		el.setAttribute("scale","0 0 0");
 		//el.setAttribute("position",data.position.x+" "+(data.position.y+(0.02872+data.padding)/2)+" "+data.position.z);
 		el.addEventListener('animationcomplete', this.handleVideoanimationcomplete);					
 	},
 	update: function (oldData) {	
 		var data=this.data,el=this.el,evil=this.evil,diff=AFRAME.utils.diff(oldData,data),changedKeys=Object.keys(diff);
 		if(changedKeys[0]=="content"){
+			el.setAttribute("scale","1 1 1");
 			el.setAttribute("geometry","primitive:plane;width:auto;height:auto");
 			el.setAttribute("text","value",data.content);	
 			var geomAtt=el.getAttribute("geometry"),
