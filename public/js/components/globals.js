61873cb84b110324d05bb80c490d7316f21f620c
Mangosh <leftpicture@gmail.com>
2019-02-21 04:56:50 +0000

    fixing load

--- public/js/components/globals.js
+++ public/js/components/globals.js
@@ -1,6 +1,6 @@
 //TODO: check how it works if you select category then while it flies cut off internet so the post fails, it sohuld retry every 10 seconds the post //while in chamber
 var socket,evil,outterRing,player,scene,assets,cam,arrows,floor,chamber,projects,projectVideo,numOfProjects,numOfImages,catLoad,projectHasVideo=false,animCounter=0,category=0,projectIndex=-1,exiting=false,projectShown=false,
-originalPosition="-0.8 0.2 2.5";
+originalPosition="-0.8 1.8 2.5";
 socket = io();
 socket.on('projectsData', function (data) {
 	projects=data;
@@ -11,6 +11,7 @@ socket.on('projectVideo', function (data) {
 	projectVideo=data.toString();
 	evil.changeVideo(projectVideo);
 });
+
 function enteringVR(){
 //we entering VR
 }
@@ -18,41 +19,41 @@ function exitingVR(){
 	console.log("exiting vr");
 	cam.setAttribute("position","0 1.6 0");
 }
-function setupfunc(){
-	evil=document.querySelector('[evil]');	
+
+function initfunc() {	
+	evil=document.querySelector('[evil]');
 	if(evil!==null){
-		evil=document.querySelector('[evil]').components.evil;	
+		outterRing=document.querySelector('#outter-ring');
+		player=document.querySelector('#player');
+		player.setAttribute("position",originalPosition);
+		cam=player.querySelector('#main-camera');
+		assets=document.querySelector('a-assets');
+		scene=document.querySelector('a-scene');
+		scene.addEventListener('enter-vr',enteringVR);
+		scene.addEventListener('exiting-vr',exitingVR);		
+		chamber=document.querySelector("#chamber");
+		arrows=document.querySelector("#main-scene-arrows");
+		floor=document.querySelector("#floor");
+		evil=document.querySelector('[evil]').components.evil;
+		resize();
 	}else{
-		setTimeout(setupfunc,20);
+		setTimeout(initfunc,100);
 	}
-	cam=document.querySelector('#main-camera');
-	resize();	
-	outterRing=document.querySelector('#outter-ring');
-	player=document.querySelector('#player');	
-	assets=document.querySelector('a-assets');
-	chamber=document.querySelector("#chamber");
-	arrows=document.querySelector("#main-scene-arrows");
-	floor=document.querySelector("#floor");
-	player.setAttribute("position",originalPosition);
-}
-function initfunc() {		
-	scene=document.querySelector("a-scene");
-	scene.addEventListener('enter-vr',enteringVR);
-	scene.addEventListener('exit-vr',exitingVR);	
-	scene.addEventListener('loaded',setupfunc);	
 }
 function resize() {
-  var hi=window.innerHeight,wi=window.innerWidth;  
+  var hi=window.innerHeight,wi=window.innerWidth;
+  if(cam!==undefined){
   if (wi>hi){
   	if(wi<768)
   	{
-  		cam.setAttribute("fov","50");
+  		cam.setAttribute("fov","40");
   	}else{  		
   		cam.setAttribute("fov","70");
   	}
   }else{
   		cam.setAttribute("fov","80");
   }
+}
 }
 window.onload = initfunc;
 window.onresize = resize;
